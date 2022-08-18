a3f79cb304a3aef61635f3925a454e8b219ec919
ibktommy <atomquartz@gmail.com>
2022-08-18 15:37:27 +0100

    Create components

---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <h2>Modal Component</h2>
+  )
+}
+
+export default Modal
---
+++ src/components/QuizForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuizForm = () => {
+  return (
+    <h2>Quiz Form</h2>
+  )
+}
+
+export default QuizForm
