0a42ab5cd96b54a314d8695f336de1adac568ec7
ibktommy <atomquartz@gmail.com>
2022-08-19 15:45:59 +0100

    Destructuring Objects from the questions array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from './context';
 import QuizForm from './components/QuizForm'
 import Loading from './components/Loading';
+import Modal from './components/Modal';
 
 function App() {
   // Getting States Needed from useGloablContext
@@ -15,9 +16,15 @@ function App() {
       return <Loading />
     }
 
+  console.log(questions)
+
+  // Destructuring Objects from the questions array
+  const { question, incorrect_answers, correct_answer } = questions[0]
+  const answers = [...incorrect_answers, correct_answer]
+  
     return (
       <main>
-        Quiz App
+        <Modal />
       </main>
     )
 
