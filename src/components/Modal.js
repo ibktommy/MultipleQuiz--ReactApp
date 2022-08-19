d5565e083ac47e57c7320a140071a5447e3215d7
ibktommy <atomquartz@gmail.com>
2022-08-19 17:34:39 +0100

    Calculate the percentage of the correctly-answered questions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,12 +5,15 @@ const Modal = () => {
   // Getting States from the Context Provider
   const { openModal, closeModalHandler, correct, questions } = useGlobalContext()
 
+  // Calculating Correct Answers in Percentage
+  const answerPercentage = `${((correct / questions.length) * 100).toFixed(0)}%`
+
   return (
     <div className={`${openModal ? 'modal-container isOpen' : 'modal-container'}`}>
       <div className="modal-content">
         <h2>Congratulations</h2>
         <p>
-          You answered {correct} question(s) out of {questions.length - 1}
+          You answered {answerPercentage} of questions correctly
         </p>
         <button onClick={closeModalHandler} className="close-btn">
           Play Again
