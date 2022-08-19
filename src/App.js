d68f885d2b18a524119f0a641ee65fead3ec11da
ibktommy <atomquartz@gmail.com>
2022-08-19 12:48:18 +0100

    Destructure out the states that are in context-file to the APpJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useGlobalContext } from './context';
+import QuizForm from './components/QuizForm'
 
 function App() {
-  return (
-    <h1>Multiple Quiz - React App</h1>
-  );
+  // Getting States Needed from useGloablContext
+  const { waiting, loading, questions, index, correct } = useGlobalContext()
+
+    if (waiting ) {
+      return <QuizForm />
+    }
+
+    if (loading) {
+      return <loading />
+    }
+
+    return (
+      <main>
+        Quiz Ap
+      </main>
+    )
+
 }
 
 export default App;
