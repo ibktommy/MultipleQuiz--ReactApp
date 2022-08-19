42665f05234667906c3f68a291c1ea475ba01a67
ibktommy <atomquartz@gmail.com>
2022-08-19 12:37:42 +0100

    Surrounding the whole App with a context-provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppProvider } from './context';
 
 ReactDOM.render(
-  <App />,
+  <AppProvider>
+    <App />
+  </AppProvider>,
   document.getElementById('root')
 );
